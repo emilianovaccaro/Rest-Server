b42990dd77979eac26962201657a3a6a41c92325
emilianovaccaro <91967480+emilianovaccaro@users.noreply.github.com>
2022-07-07 16:38:13 -0300

    Delete keys

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-//KEYS NOT TO COMMIT
-module.exports = {
-  googleClientId: "699918039755-ifn0dj9jtsrd64u29fgusoh1919mo9po.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI: "mongodb+srv://Emiliano192:Asd123!!@cluster0.joxws.mongodb.net/database",
-  cookieKey: "asd21231sdasdgqreqefasdwqeadzxcnuethgjuk"
-};
